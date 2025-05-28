10faa7c9a6ecc4005da92899464a11140749f106
cnriveraa <rivera.cn18@gmail.com>
2025-05-28 14:48:45 +0800

    added save_to_file function

--- python_quiz_creator/python_quiz.py
+++ python_quiz_creator/python_quiz.py
@@ -8,3 +8,12 @@ class Quiz:
             'answers': answers,
             'correct_answer': correct_answer
         })
+
+    def save_to_file(self, file_name):
+        with open(file_name, 'w') as file:
+            for question in self.questions:
+                file.write(f"Q: {question['question']}\n")
+                for key, answer in question['answers'].items():
+                    file.write(f"{key}: {answer}\n")
+                file.write(f"Correct Answer: {question['correct_answer']}\n")
+                file.write("\n")
