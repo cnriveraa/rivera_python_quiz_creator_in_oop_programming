d2f7f6eb5bb057525d2fbcf91b786b776b1f2f6c
cnriveraa <rivera.cn18@gmail.com>
2025-05-28 14:37:45 +0800

    function add_question added

--- python_quiz_creator/python_quiz.py
+++ python_quiz_creator/python_quiz.py
@@ -1,3 +1,10 @@
 class Quiz:
     def __init__(self):
         self.questions = []
+
+    def add_question(self, question, answers, correct_answer):
+        self.questions.append({
+            'question': question,
+            'answers': answers,
+            'correct_answer': correct_answer
+        })
