d484bf4b817548cc0077aa3547f681ed06da740d
cnriveraa <rivera.cn18@gmail.com>
2025-05-28 15:49:05 +0800

    fix: remove redundant save_quiz call from main execution script

--- python_quiz_creator/main.py
+++ python_quiz_creator/main.py
@@ -4,4 +4,3 @@ from quiz_creator import QuizCreator
 
 quiz_creator = QuizCreator()  # create an instance of QuizCreator
 quiz_creator.create_quiz()  # start creating the quiz
-quiz_creator.save_quiz()  # save the quiz to a file
