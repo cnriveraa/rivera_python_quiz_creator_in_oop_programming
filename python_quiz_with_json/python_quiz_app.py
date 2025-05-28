71bcd31c8e54077478080dd310faf0025215dcf3
cnriveraa <rivera.cn18@gmail.com>
2025-05-28 16:58:05 +0800

    ensured questions are loaded correctly and handle file existence check

--- python_quiz_with_json/python_quiz_app.py
+++ python_quiz_with_json/python_quiz_app.py
@@ -6,3 +6,8 @@ class BaseQuiz:
     def __init__(self, quiz_output):
         self.quiz_output = quiz_output
         self.questions = self.load_questions()
+
+    def load_questions(self):
+        if not os.path.exists(self.quiz_output):
+            print(f"Error: File '{self.quiz_output}' does not exist.")
+            return []
