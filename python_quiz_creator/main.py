855daac982ae2e7e85f68a1535b38cdfda89cd3d
cnriveraa <rivera.cn18@gmail.com>
2025-05-28 16:01:01 +0800

    fix: restructure main execution to ensure proper quiz creation flow

--- python_quiz_creator/main.py
+++ python_quiz_creator/main.py
@@ -2,5 +2,7 @@
 
 from quiz_creator import QuizCreator
 
-quiz_creator = QuizCreator()  # create an instance of QuizCreator
-quiz_creator.create_quiz()  # start creating the quiz
+if __name__ == "__main__":
+    quiz_creator = QuizCreator()  # create an instance of QuizCreator
+    quiz_creator.create_quiz()  # start creating the quiz
+    quiz_creator.save_quiz()  # save the created quiz to a file
